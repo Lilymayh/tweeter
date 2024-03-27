dd588e2207c023e0b951b0b5c8eddd159d8f76c9
Dicky jr <lmayhenderson@yahoo.com>
2024-03-27 14:07:47 -0700

    Implement js logic to generate the DOM structure for a tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,36 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+	"user": {
+		"name": "Newton",
+		"avatars": "https://i.imgur.com/73hZDYK.png",
+			"handle": "@SirIsaac"
+		},
+	"content": {
+			"text": "If I have seen further it is by standing on the shoulders of giants"
+		},
+	"created_at": 1461116232227
+}
 
+const renderTweets = function(tweets) {
+  tweets.forEach(tweet => {
+		const $tweet = createTweetElement(tweet)
+
+		$('#tweets-container').append($tweet)
+	})
+}
+
+const createTweetElement = function(tweet) {
+  let $tweet = $('<article>').addClass('tweet')
+	const createdAt = new Date(tweet.created_at)
+
+	$tweet.append(`<header><img src="${tweet.user.avatars}"><h3>${tweet.user.name}</h3><span>${tweet.user.handle}</span></header>`);
+  $tweet.append(`<p>${tweet.content.text}</p>`);
+  $tweet.append(`<footer><span>${createdAt.toDateString()}</span></footer>`);
+
+  return $tweet;
+}
+
+renderTweets(data);
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('.new-tweet textarea').on('input', function(event) {
+	$('.new-tweet textarea').on('keyup', function(event) {
 		const textarea = $(this);
 		//traverse up the dom > then find .counter
 		const counter = textarea.closest('.new-tweet').find('.counter');
@@ -12,9 +12,9 @@ $(document).ready(function() {
 
 		//add or remove styling class based on the character length =>
 		if (remainingLength < 0) {
-			counter.addClass('countChars');
+			counter.addClass('negativeCount');
 		} else {
-			counter.removeClass('countChars');
+			counter.removeClass('negativeCount');
 		}
 	});
 });
