7f3945f248356627566721c19afe6154477a5681
Dicky jr <lmayhenderson@yahoo.com>
2024-03-26 18:23:06 -0700

    Registered an event handler to update  the character count when typing in textarea

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	$('.new-tweet textarea').on('input', function(event) {
+		const textarea = $(this);
+		//traverse up the dom > then find .counter
+		const counter = textarea.closest('.new-tweet').find('.counter');
+
+		const maxTweet = 140;
+		const currentLength = textarea.val().length;
+		const remainingLength = maxTweet - currentLength;
+
+		counter.text(remainingLength);
+
+		//add or remove styling class based on the character length =>
+		if (remainingLength < 0) {
+			counter.addClass('countChars');
+		} else {
+			counter.removeClass('countChars');
+		}
+	});
+});
+
+
